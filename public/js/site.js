a6e1e33b2d10756071ae84adc3cf50bb80fcafc9
Victor <victor.a.s@yandex.ru>
2016-12-20 00:38:53 +0700

    22575:: Init

---
+++ public/js/site.js
@@ -0,0 +1,5 @@
+(function MM_preloadImages(){var d=document;var p=d.MM_p=d.MM_p||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}})('img/header_background.jpg');
+
+window.onload = function () {
+	document.body.classList.remove('loading');
+};
