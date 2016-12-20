6caa591de06b7222e3edb531dd03eab31c00d085
Victor <victor.a.s@yandex.ru>
2016-12-20 22:48:54 +0700

    22575:: Landing update 2

--- public/js/site.js
+++ public/js/site.js
@@ -1,4 +1,4 @@
-(function MM_preloadImages(){var d=document;var p=d.MM_p=d.MM_p||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}})('img/header_background.jpg');
+(function(){var d=document,p=d.picLdr=d.picLdr||[];for(var i=0,a=arguments;i<a.length;i++){var x=new Image();x.src=a[i];p.push(x);}})('img/pg_top.jpg', 'img/pg_bottom.jpg');
 
 window.onload = function () {
 	document.body.classList.remove('loading');
